d08998411cffc286ac915d7c608bcb7fd627768d
Ezzatulla <ahmadi.ez4t@gmail.com>
2020-03-31 21:28:39 +1100

    another commit-a

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,6 @@ module.exports = {
     author: `Ezzatulla`,
   },
   plugins: [
-    " gatsby-transformer-sharp",
     // Styled Components
     `gatsby-plugin-styled-components`,
     // Tailwind Css
